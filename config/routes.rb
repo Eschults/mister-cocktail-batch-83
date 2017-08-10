3faa4ab5448dc4372cb5d9cde7f065b3764ab9ac
Edward Schults <edward.schults@gmail.com>
2017-08-10 18:54:18 +0200

    end of livecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    resources :cocktails, only: [:show, :index, :new, :create]
-    root "cocktails#index"
+  resources :cocktails, only: [:show, :index, :new, :create] do
+    resources :doses, only: [:create]
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources(:doses, { only: [:destroy]})
+
+  root "cocktails#index"
 end
